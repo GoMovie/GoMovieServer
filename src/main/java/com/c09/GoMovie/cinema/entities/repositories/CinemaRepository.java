73dd5f918da713b9483bc545dbc28e170d96470c
Jackie6 <541172791@qq.com>
2017-06-16 19:37:17 +0800

    add interface to get cinema by movie

--- src/main/java/com/c09/GoMovie/cinema/entities/repositories/CinemaRepository.java
+++ src/main/java/com/c09/GoMovie/cinema/entities/repositories/CinemaRepository.java
@@ -2,11 +2,12 @@ package com.c09.GoMovie.cinema.entities.repositories;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import com.c09.GoMovie.cinema.entities.Cinema;
 import com.c09.GoMovie.cinema.entities.CinemaComment;
 
-public interface CinemaRepository extends CrudRepository<Cinema, Long> {
+public interface CinemaRepository extends JpaRepository<Cinema, Long> {
 	List<Cinema> findByCityId(int cityId);
 }
--- src/main/java/com/c09/GoMovie/cinema/entities/repositories/SeatRepository.java
+++ src/main/java/com/c09/GoMovie/cinema/entities/repositories/SeatRepository.java
@@ -2,10 +2,11 @@ package com.c09.GoMovie.cinema.entities.repositories;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import com.c09.GoMovie.cinema.entities.Seat;
 
-public interface SeatRepository extends CrudRepository<Seat, Long> {
+public interface SeatRepository extends JpaRepository<Seat, Long> {
 	List<Seat> findByHallId(long id);
 }
